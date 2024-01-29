13d5f3f04b16e2011bf2712951ec2cbab7beeb20
iamlydial <lydia.lavecchia@gmail.com>
2024-01-29 22:04:54 +0100

    card component started

--- src/app/components/song-card/song-card.component.ts
+++ src/app/components/song-card/song-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-song-card',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './song-card.component.css'
 })
 export class SongCardComponent {
+@Input() public playlistThumbnail!: string;
+@Input() public title!: string;
+@Input() public description!: string;
 
+constructor(){}
+
+ngOnInit(): void{}
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { TopNavComponent } from '../../components/top-nav/top-nav.component';
+import { SongCardComponent } from '../../components/song-card/song-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TopNavComponent],
+  imports: [TopNavComponent, SongCardComponent, CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  public songCards = [
+    {
+      thumbnail: '',
+      title: 'Romantic Songs',
+      description: 'Relax and indulge with beautiful piano pieces',
+    },
+    {
+      thumbnail: '',
+      title: 'Upbeat Hits',
+      description: 'Get energized with the latest upbeat tracks',
+    },
+    {
+      thumbnail: '',
+      title: 'Chill Vibes',
+      description: 'Unwind with a collection of chill and laid-back tunes',
+    },
+    {
+      thumbnail: '',
+      title: 'Workout Jams',
+      description: 'Power through your workout with high-energy songs',
+    },
+    {
+      thumbnail: '',
+      title: 'Study Focus',
+      description: 'Create a focused atmosphere with instrumental study music',
+    },
+    {
+      thumbnail: '',
+      title: 'Throwback Classics',
+      description: 'Take a trip down memory lane with timeless classics',
+    },
+  ];
 }
